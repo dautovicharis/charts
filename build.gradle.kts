083864b6309a441ac5441f76611f58dacc07a62a
Haris Dautović <haris.dautovic.dev@gmail.com>
2025-09-29 21:35:33 +0200

    Remove SonarQube integration and related configurations from build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,8 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.build.config) apply false
-    alias(libs.plugins.sonarqube) apply true
     alias(libs.plugins.kover) apply true
     alias(libs.plugins.compose.compiler) apply false
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "dautovicharis_Charts")
-        property("sonar.organization", "dautovicharis")
-        property("sonar.host.url", "https://sonarcloud.io/")
-        property(
-            "sonar.coverage.jacoco.xmlReportPaths",
-            "${project.rootDir}/charts/build/reports/kover/report.xml," //+
-                    //"${project.rootDir}/app/build/reports/kover/report.xml"
-        )
-        property("sonar.coverage.exclusions", "**/preview/**")
-        property("sonar.exclusions", "**/app/**")
-    }
 }
 
 tasks.register("chartsTest") {
@@ -39,15 +23,13 @@ tasks.register("chartsTest") {
 
 tasks.register("chartsCheck") {
     group = "Charts"
-    description = "Build, tests, coverage report and sonar analysis for the charts project"
+    description = "Build, tests and coverage report for the charts project"
     dependsOn("build")
     dependsOn("chartsTest")
     dependsOn("charts:koverXmlReport")
-    dependsOn("sonar")
 
     tasks.findByName("chartsTest")?.mustRunAfter("build")
     tasks.findByName("charts:koverXmlReport")?.mustRunAfter("chartsTest")
-    tasks.findByName("sonar")?.mustRunAfter("charts:koverXmlReport")
 }
 
 tasks.register("generateJsDemo") {
