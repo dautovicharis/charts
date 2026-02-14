9f16ac5b0b11decb807355208cbbde491c989601
HDCode <hdcodedev@gmail.com>
2026-02-14 15:09:36 +0100

    Fix ktlint

--- charts-stacked-bar/src/commonMain/kotlin/io/github/dautovicharis/charts/internal/barstackedchart/StackedBarChart.kt
+++ charts-stacked-bar/src/commonMain/kotlin/io/github/dautovicharis/charts/internal/barstackedchart/StackedBarChart.kt
@@ -24,8 +24,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
