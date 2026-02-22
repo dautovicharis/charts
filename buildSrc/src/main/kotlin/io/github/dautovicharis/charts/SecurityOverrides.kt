7d60ca7ed525a93e13f8cde19030f84af4e6a4f6
HDCode <hdcodedev@gmail.com>
2026-02-22 05:36:38 +0100

    fix(deps): pin jdom2 transitive to 2.0.6.1

--- buildSrc/src/main/kotlin/io/github/dautovicharis/charts/SecurityOverrides.kt
+++ buildSrc/src/main/kotlin/io/github/dautovicharis/charts/SecurityOverrides.kt
@@ -9,4 +9,8 @@ object SecurityOverrides {
             "protobuf-kotlin-lite",
         )
     const val protobufReason = "Mitigate CVE-2024-7254 / GHSA-735f-pc8j-v9w8"
+
+    const val jdomGroup = "org.jdom"
+    const val jdomArtifact = "jdom2"
+    const val jdomReason = "Mitigate XXE in org.jdom:jdom2 < 2.0.6.1"
 }
